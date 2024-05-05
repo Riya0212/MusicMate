acc43514f6fc3ab2d8211dd7edf2ca642f8b5ffd
Riya Patel <ria020601@gmail.com>
2024-05-05 23:09:45 +0530

    added firebase setup and renamed app name

--- android/app/src/main/java/com/tempelateapp/MainActivity.kt
+++ android/app/src/main/java/com/tempelateapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tempelateapp
+package com.musicmate
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "TempelateApp"
+  override fun getMainComponentName(): String = "MusicMate"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
