6bcf0ba68799a0764927bbb61fdc6d3950c20303
Dhanraj Mahida <119103043+dhanra1509@users.noreply.github.com>
2024-05-07 20:37:08 +0530

    changes in icon and styles in login screen

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -41,15 +41,15 @@ function Login({navigation}) {
             <View  style={styles.header}>
                 <View style={{justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center'}}>
                     <View style={{flex: 1}}/>
-                    <Image source={icon} style={{width: wp(35), height: hp(8)}} resizeMode='contain' />
+                    <Image source={icon} style={{width: wp(35), height: wp(35)}} resizeMode='contain' />
                     <View style={{flex: 1}}/>
                 </View>
-                <View style={{alignSelf: 'center', alignItems: 'center', width: '80%', marginTop: hp(5)}}>
+                <View style={{alignSelf: 'center', alignItems: 'center', width: '80%'}}>
                     <Text style={{fontFamily: 'Satoshi-Bold', fontSize: 30, color: '#000', marginVertical: wp(5)}}>Sign In</Text>
                     <View style={{flexDirection: 'row'}}>
                         <Text style={{fontFamily: 'Satoshi-Regular', color: '#383838'}}>If You Need Any Support </Text>
                         <TouchableOpacity onPress={() => navigation.navigate(NAVIGATION.register)} >
-                            <Text style={{fontFamily: 'Satoshi-Regular',  color: '#38B432'}}>Click Here</Text>
+                            <Text style={{fontFamily: 'Satoshi-Regular',  color: '#AC261B'}}>Click Here</Text>
                         </TouchableOpacity>
                     </View>
                     <TextInputIconComponent
@@ -71,7 +71,7 @@ function Login({navigation}) {
                         }}
                         sectionStyle={styles.sectionStyle}
                     />
-                    <TouchableOpacity style={{width: '100%', marginTop: hp(3), paddingLeft: wp(5)}}>
+                    <TouchableOpacity style={{width: '100%', marginTop: hp(2), paddingLeft: wp(5)}}>
                         <Text style={{fontFamily: 'Satoshi-Medium', fontSize: 14, color: '#383838'}}>Recovery Password</Text>
                     </TouchableOpacity>
                     <ButtonComponent 
@@ -137,19 +137,19 @@ const styles = StyleSheet.create({
         color: '#383838',
         fontFamily: 'Satoshi-Medium',
         fontSize: 16,
-        paddingVertical: wp(4),
+        paddingVertical: wp(3),
         paddingLeft: wp(5),
         borderColor: '#D9D9D9',
         borderRadius: wp(6)
     },
     buttonContainer: {
         width: '100%',
-        backgroundColor: '#38B432',
+        backgroundColor: '#AC261B',
         marginTop: hp(3),
         marginBottom: hp(6),
         borderRadius: 25,
         paddingHorizontal: wp(10),
-        paddingVertical: wp(6)
+        paddingVertical: wp(5)
     },
     buttontext: {
         fontFamily: 'Satoshi-Bold',
